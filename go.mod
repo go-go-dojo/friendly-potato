47a06ff06cb02079ab18f13687dbba48466f6551
Erick Lopes <el@cooldex.com>
2020-08-31 10:15:56 -0300

    init api connection

---
+++ go.mod
@@ -0,0 +1,5 @@
+module friendly-potato
+
+go 1.15
+
+require github.com/cloudflare/cloudflare-go v0.13.1
---
+++ integrations/cloudflare.go
@@ -0,0 +1,13 @@
+package integrations
+
+import "github.com/cloudflare/cloudflare-go"
+
+var api cloudflare.API
+
+func InitAPI(apiKey, apiEmail string)(err error){
+	api,err=cloudflare.New(apiKey,apiEmail)
+	if err!=nil{
+		return err
+	}
+	return nil
+}
