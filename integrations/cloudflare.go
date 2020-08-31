c1f9f2fdffd76b8a9445d54087e8de467f44f7db
Robinson Donizete de Carvalho <robinson.carvalho@symphony.com>
2020-08-31 10:46:09 -0300

    add list zones

--- integrations/cloudflare.go
+++ integrations/cloudflare.go
@@ -1,13 +1,26 @@
 package integrations
 
-import "github.com/cloudflare/cloudflare-go"
+import (
+	"github.com/cloudflare/cloudflare-go"
+	cf "github.com/cloudflare/cloudflare-go"
+)
 
-var api cloudflare.API
+var api *cloudflare.API
 
-func InitAPI(apiKey, apiEmail string)(err error){
-	api,err=cloudflare.New(apiKey,apiEmail)
-	if err!=nil{
+// InitAPI -- Configure token to cloudflare
+func InitAPI(apiToken string) (err error) {
+	api, err = cloudflare.NewWithAPIToken(apiToken)
+	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func HealthCheck() {
+	//api.Raw("", endpoint string, data interface{}) (json.RawMessage, error));
+}
+
+// ListZones -- get all zones
+func ListZones() ([]cf.Zone, error) {
+	return api.ListZones()
 }
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"friendly-potato/integrations"
+	"log"
+)
+
 func main() {
-	
+	fmt.Println("teste")
+	err := integrations.InitAPI("OpmWl7p_ECwb1U2YMVlSXhqFW2017_we9lMCQ_4V")
+
+	if err != nil {
+		log.Fatalf("Fatal on auth %v", err)
+	}
+
+	zones, err := integrations.ListZones()
+	fmt.Printf("%v", zones)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
